245f162c4606f358b3a001038a5af33a124758a9
Dmitri Chirov <dim.ch.it80@gmail.com>
2023-03-26 16:37:35 +0300

    edit file funcs.py with append function difference of numbers

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def sum_of_two_numbers(number_1, number_2):
     '''gets two numbers and returns their sum'''
     return number_1 + number_2
+
+
+def dif_of_two_numbers(number_1, number_2):
+    '''gets two numbers and returns their difference. Number_1 - number_2'''
+    return number_1 - number_2
